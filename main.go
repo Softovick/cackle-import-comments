e3e0c893a26979f661a6b0fc95a8321b487b7db3
Aleksej Kolosov <akolosov2012@yandex.ru>
2022-02-19 18:06:45 +0300

    Добавил чтение и сохранение комментариев по списку каналов

--- main.go
+++ main.go
@@ -14,12 +14,38 @@ import (
 )
 
 type Channel struct {
-	Id      uint64 `json:"id"`
+	Id      int    `json:"id"`
 	Channel string `json:"channel"`
 	Url     string `json:"url"`
 	Title   string `json:"title"`
-	Created uint64 `json:"created"`
+	Created int64  `json:"created"`
 	Count   int    `json:"count"` //вообще там всегда вроде 0, но добавил для совместимости
+}
+
+type Author struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Avatar   string `json:"avatar"`
+	Www      string `json:"www"`
+	Provider string `json:"provider"`
+	OpenID   string `json:"openId"`
+	Verify   bool   `json:"verify"`
+	Notify   bool   `json:"notify"`
+}
+
+type Comment struct {
+	ID       int     `json:"id"`
+	SiteID   int     `json:"siteId"`
+	ParentID int     `json:"parentId"`
+	Path     []int   `json:"path"`
+	Message  string  `json:"message"`
+	Rating   int     `json:"rating"`
+	Status   string  `json:"status"`
+	Created  int64   `json:"created"`
+	Author   Author  `json:"author"`
+	Chan     Channel `json:"chan"`
+	IP       string  `json:"ip"`
+	Modified string  `json:"modified"`
 }
 
 func main() {
@@ -33,13 +59,13 @@ func main() {
 	if err != nil || timeout <= 0 {
 		timeout = 5
 	}
-
+	// читаем каналы (по сути это список страниц, к которым есть комментарии)
 	apiUrlChannels := fmt.Sprintf("http://cackle.me/api/3.0/comment/chan/list.json?id=%s&siteApiKey=%s&accountApiKey=%s",
 		os.Getenv("ID"), os.Getenv("SITE_API_KEY"), os.Getenv("ACCOUNT_API_KEY"))
 	//log.Println(apiUrlChannels)
 	page := 0
 	var allChannels []Channel
-	for {
+	for { //цикл по получению списка каналов, следуя рекомендации сервиса запросы делаются с паузами и по 100 шт
 		count, channels, err := getChannels(apiUrlChannels, page)
 		if err != nil {
 			log.Fatal(err)
@@ -53,7 +79,7 @@ func main() {
 		time.Sleep(time.Duration(timeout) * time.Second)
 	}
 	log.Printf("All channels count %d\n", len(allChannels))
-	log.Printf("Saving all channels to file...\n")
+	log.Printf("Saving all channels to file...")
 
 	file, err := json.MarshalIndent(allChannels, "", " ")
 	if err != nil {
@@ -63,6 +89,51 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Printf("Done\n")
+	//читаем комментарии
+	log.Println("Getting comments...")
+	apiUrlComments := fmt.Sprintf("http://cackle.me/api/3.0/comment/list.json?id=%s&siteApiKey=%s&accountApiKey=%s",
+		os.Getenv("ID"), os.Getenv("SITE_API_KEY"), os.Getenv("ACCOUNT_API_KEY"))
+	var allComments []Comment
+	for index, channel := range allChannels {
+		log.Printf("Get comments from channel id %d (%d from %d),\n URL: %s\n Title: %s\n", channel.Id, index, len(allChannels), channel.Url, channel.Title)
+		commentId := 0
+		currentCount := 0
+		for {
+			time.Sleep(time.Duration(timeout) * time.Second)
+			count, comments, err := getComments(apiUrlComments, channel.Channel, commentId)
+			log.Println("Get block with count: ", count)
+			currentCount += count
+			if err != nil {
+				log.Println(err)
+				break
+			} else {
+				allComments = append(allComments, comments...)
+				if count < 100 {
+					commentId = 0
+				} else {
+					commentId = comments[99].ID
+				}
+			}
+			if commentId == 0 {
+				break
+			}
+		}
+		log.Printf("Count comments: %d", currentCount)
+		currentCount = 0
+	}
+	log.Println("Done...")
+	log.Printf("All comments count %d\n", len(allComments))
+	log.Printf("Saving all comments to file...")
+	file, err = json.MarshalIndent(allComments, "", " ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = ioutil.WriteFile("comments.json", file, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Done...")
 }
 
 func getChannels(baseApiUrl string, page int) (count int, channels []Channel, err error) {
@@ -71,15 +142,33 @@ func getChannels(baseApiUrl string, page int) (count int, channels []Channel, er
 	}
 	response, err := http.Get(baseApiUrl)
 	if err != nil {
-		return 0, []Channel{}, err
+		return 0, nil, err
 	}
 	var result map[string][]Channel
 	err = json.NewDecoder(response.Body).Decode(&result)
 	if err != nil {
-		return 0, []Channel{}, err
+		return 0, nil, err
 	}
 	count = len(result["chans"])
 	channels = result["chans"]
+	err = nil
+	return
+}
+
+func getComments(baseApiUrl string, channel string, commentId int) (count int, comments []Comment, err error) {
+	baseApiUrl = baseApiUrl + fmt.Sprintf("&chan=%s", channel) + fmt.Sprintf("&commentId=%s", commentId)
+
+	response, err := http.Get(baseApiUrl)
+	if err != nil {
+		return 0, nil, err
+	}
+	var result map[string][]Comment
+	err = json.NewDecoder(response.Body).Decode(&result)
+	if err != nil {
+		return 0, nil, err
+	}
+	count = len(result["comments"])
+	comments = result["comments"]
 	err = nil
 	return
 }
